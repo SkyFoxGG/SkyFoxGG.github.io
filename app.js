40915d190147721c04828277685a926f93643d07
SkyFoxGG <vsootor@gmail.com>
2025-03-07 20:25:08 +0300

    SkyFoxGG

--- app.js
+++ app.js
@@ -6,7 +6,13 @@
 const registrationBtn = document.getElementById("registration-btn");
 const myModal = document.getElementById("myModal");
 const closeBtn = document.getElementById("close-btn");
+const moreBtn = document.getElementById("more");
+const imageHidden = document.getElementById("image-hidden");
+const registrationMenuBtn = document.getElementById("registration-menu-btn");
+const menuModal = document.getElementById("menu-modal");
+const closeMenuBtn = document.getElementById("close-menu-btn");
 
+if (registrationBtn){
 registrationBtn.addEventListener("click", function () {
     console.log("Кнопка нажата"); // Добавим для отладки
     console.log("Текущий display:", myModal.style.display); // Добавим для отладки
@@ -17,8 +23,10 @@ registrationBtn.addEventListener("click", function () {
         myModal.style.display = "none";
     }
 });
+}
 
-closeBtn.addEventListener("click", function () {
+if (closeBtn){
+    closeBtn.addEventListener("click", function () {
     console.log("Кнопка нажата"); // Добавим для отладки
     console.log("Текущий display:", myModal.style.display); // Добавим для отладки
 
@@ -28,11 +36,47 @@ closeBtn.addEventListener("click", function () {
         myModal.style.display = "block";
     }
 });
+}
 
+if (moreBtn){
+moreBtn.addEventListener("click", function () {
+    console.log("Кнопка нажата"); // Добавим для отладки
+    console.log("Текущий display:", imageHidden.style.display); // Добавим для отладки
 
+    if (imageHidden.style.display !== "flex") {
+        imageHidden.style.display = "flex";
+        moreBtn.style.display = "none";
+    } else {
+        imageHidden.style.display = "none";
+    }
+});
+}
 
+if (registrationMenuBtn){
+    registrationMenuBtn.addEventListener("click", function () {
+        console.log("Кнопка нажата"); // Добавим для отладки
+        console.log("Текущий display:", menuModal.style.display); // Добавим для отладки
+    
+        if (menuModal.style.display !== "block") {
+            menuModal.style.display = "block";
+        } else {
+            menuModal.style.display = "none";
+        }
+    });
+    }
 
-
+    if (closeMenuBtn){
+        closeMenuBtn.addEventListener("click", function () {
+        console.log("Кнопка нажата"); // Добавим для отладки
+        console.log("Текущий display:", menuModal.style.display); // Добавим для отладки
+    
+        if (menuModal.style.display !== "none") {
+            menuModal.style.display = "none";
+        } else {
+            menuModal.style.display = "block";
+        }
+    });
+    }
 
 
 
